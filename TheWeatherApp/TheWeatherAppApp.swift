382abdcb78f9790292155c7149d0b6568141d653
AdithyaR9 <rocket_saturn@hotmail.com>
2024-03-16 12:23:37 -0500

    Initial Commit

---
+++ TheWeatherApp/TheWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheWeatherAppApp.swift
+//  TheWeatherApp
+//
+//  Created by Adithya Ramanathan on 3/16/24.
+//
+
+import SwiftUI
+
+@main
+struct TheWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
